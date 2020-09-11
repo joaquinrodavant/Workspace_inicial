c67521b73c115e6e449d9e6b9b3f62c10e360a48
joaquinrodavant <68909994+joaquinrodavant@users.noreply.github.com>
2020-09-11 12:47:18 -0300

    Entregable 3.1

--- js/init.js
+++ js/init.js
@@ -46,5 +46,5 @@ var getJSONData = function(url){
 document.addEventListener("DOMContentLoaded", function(e){
 });
 //Mosrtar nombre de usuario
-var user = localStorage.getItem("datos");
+var user = localStorage.getItem("Dirección correo electronico");
 document.getElementById("user").innerHTML = "Bienvenido: " + user
